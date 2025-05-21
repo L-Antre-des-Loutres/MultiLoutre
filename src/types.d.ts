ff3c2cb599434519f5a60bab63557b05e78d92d3
matheo-1712 <perod.matheo@gmail.com>
2025-05-21 22:05:56 +0200

    Correction de l'id du salon

--- src/types.d.ts
+++ src/types.d.ts
@@ -14,6 +14,7 @@ declare global {
             ROLE_NAME: string;
             
             DB_HOST: string;
+            DB_PORT: number;
             DB_USER: string;
             DB_PASSWORD: string;
             DB_NAME: string;
